e127a90956482fa1bb585bb69f8348dce128573d
Carl Caulkett <carl.caulkett@gmail.com>
2025-04-10 21:36:16 +0100

    Hello from Laminar.

---
+++ LiveChart.scala
@@ -0,0 +1,42 @@
+package livechart
+
+import com.raquo.laminar.api.L.{*, given}
+import scala.scalajs.js
+import scala.scalajs.js.annotation.*
+
+import org.scalajs.dom
+
+// import javascriptLogo from "/javascript.svg"
+@js.native @JSImport("/javascript.svg", JSImport.Default)
+val javascriptLogo: String = js.native
+
+@main
+def LiveChart(): Unit =
+  renderOnDomContentLoaded(dom.document.getElementById("app"), Main.appElement())
+
+object Main:
+  def appElement(): Element =
+    div(
+      a(href := "https://vitejs.dev", target := "_blank",
+        img(src := "/vite.svg", className := "logo", alt := "Vite logo"),
+      ),
+      a(href := "https://developer.mozilla.org/en-US/docs/Web/JavaScript", target := "_blank",
+        img(src := javascriptLogo, className := "logo vanilla", alt := "JavaScript logo"),
+      ),
+      h1("Hello from Laminar!"),
+      div(className := "card",
+        button(tpe := "button"),
+      ),
+      p(className := "read-the-docs",
+        "Click on the Vite logo to learn more",
+      ),
+    )
+
+def counterButton(): Element =
+    val counter = Var(0)
+    button(
+      tpe := "button",
+      "count is ",
+      child.text <-- counter,
+      onClick --> { event => counter.update(c => c + 1) },
+    )
